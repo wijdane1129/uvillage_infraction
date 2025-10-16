09a348f318e8ee05419f6b3e68ed042495efb2eb
wijdane1129 <lahboubwijdane@gmail.com>
2025-10-16 18:06:52 +0100

    initial commit

---
+++ backend/src/main/java/com/uvillage/infractions/UvillageInfractionsApplication.java
@@ -0,0 +1,13 @@
+package com.uvillage.infractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UvillageInfractionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UvillageInfractionsApplication.class, args);
+	}
+
+}
---
+++ frontend/android/app/src/main/kotlin/com/example/infractions_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.infractions_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
